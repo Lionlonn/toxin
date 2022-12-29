3503b93103bbd04ca24d087c3dc15e3f01fd3c7d
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2022-12-29 18:59:57 +0300

    version 1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './template.pug';
+
+import './index.scss';
+import menu3 from './img/menu-3.svg'
+import { mult, sum } from './modules/calc';
+
+
+const img = new Image();
+img.src = menu-3;
+
+
+
+var html = template (locals);
+console.log(mult(5, 4));
+console.log(sum(5, 4));
+
