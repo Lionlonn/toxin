202b2b3745b9fad607cf781189c62ee90beadc14
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-01-25 20:23:48 +0300

    dropdown-corection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './views/layout/main-page.pug';
 // import './libs/air-datapicker/datapicker';
 import './views/components/datapicker/datapicker';
 import './views/components/like-button/like';
+import './views/components/dropdown_guests/guests-drop';
 
 import './index.scss';
 
