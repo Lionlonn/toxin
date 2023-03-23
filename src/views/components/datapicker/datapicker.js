6e081ebef90aa4ef8346c8f9d434c629eb5faff2
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-03-23 15:45:56 +0300

    add: datepicker arrow

--- src/views/components/datapicker/datapicker.js
+++ src/views/components/datapicker/datapicker.js
@@ -9,8 +9,8 @@ const DATEPICKER_INPUT_SELECTOR = ".datepicker-input";
 const DROP_DATAPICKER_SELECTOR = ".datepicker";
 const AIR_DATEPICKER_SELECTOR = '.datepicker-wrapper';
 const AIR_DATEPICKER_VISIBLE_SELECTOR = "air-datepicker--opened";
-
-
+const AIR_DATEPICKER_ARROW_SELECTOR = '.input__datepicker';
+const ARROW_ANIMATION = 'animation-arrow'
 
 
 
@@ -19,9 +19,15 @@ const AIR_DATEPICKER_VISIBLE_SELECTOR = "air-datepicker--opened";
 const dropDatapicker = document.querySelector(DROP_DATAPICKER_SELECTOR);
 const dataHere = document.querySelector(AIR_DATEPICKER_SELECTOR);
 
+//arrow
+const arrowInput = document.querySelectorAll(AIR_DATEPICKER_ARROW_SELECTOR);
 
 const openDatepicker = () => {
     dataHere.classList.toggle(AIR_DATEPICKER_VISIBLE_SELECTOR);
+    arrowInput.forEach((arrowAnim) => {
+        arrowAnim.classList.toggle(ARROW_ANIMATION)
+        
+    })
 }
 
 
@@ -41,8 +47,11 @@ fields.forEach(field => field.addEventListener('click', openDatepicker));
 const outClick = document.querySelectorAll(DATEPICKER_INPUT_SELECTOR);
 document.addEventListener('click', (e) => {
     if(e.target !== outClick) {
-        
         dataHere.classList.remove(AIR_DATEPICKER_VISIBLE_SELECTOR);
+        arrowInput.forEach((arrowAnim) => {
+            arrowAnim.classList.remove(ARROW_ANIMATION)
+            
+        })
     }
 })
 
