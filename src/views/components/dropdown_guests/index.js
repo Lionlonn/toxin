d83c3a1f8804f449d8a6b81fad8e9ad3be32ca80
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-04-16 14:20:59 +0300

    corection mixin dropdown-guests

--- src/views/components/dropdown_guests/index.js
+++ src/views/components/dropdown_guests/index.js
@@ -2,9 +2,6 @@ import Dropdown from './Dropdown';
 
 
 
-
-
-
 const dropdownOptions1 = {
     dropdownSelector: '#dropdown-guests',
     dropdownListSelector: '#dropdownList1',
@@ -16,11 +13,28 @@ const dropdownOptions1 = {
             separated_values: ['Младенец', 'Младанца', 'Мледанцев'],
         }
     },
-    buttonsClearSelector: '#btnClearGuestsId',
+    buttonsClearSelector: '#clear-id',
+};
+const dropdownOptions3 = {
+    dropdownSelector: '#dropdown-booking',
+    dropdownListSelector: '#dropdown-list__booking',
+    shared_value: ['Гость', 'Гостя', 'Гостей'],
+    fields: {
+        adults: {},
+        children: {},
+        babies: {
+            separated_values: ['Младенец', 'Младанца', 'Мледанцев'],
+        }
+    },
+    buttonsClearSelector: '#clear-booking',
 };
 
 
 
-const test1 = new Dropdown(dropdownOptions1)
+
+
+
+const myDropdown1 = new Dropdown(dropdownOptions1);
+const myDropdown2 = new Dropdown(dropdownOptions3);
 
 
