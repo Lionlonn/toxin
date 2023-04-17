85ff7e8d9dd10028ff700bc88a8695b443cc30c3
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-04-17 15:42:58 +0300

    corection datepicker.js

--- src/views/components/datapicker/datapicker.js
+++ src/views/components/datapicker/datapicker.js
@@ -15,17 +15,34 @@ const ARROW_ANIMATION = 'animation-arrow';
 //Открытие календаря
 
 const dropDatapicker = document.querySelector(DROP_DATAPICKER_SELECTOR);
-const dataHere = document.querySelector(AIR_DATEPICKER_SELECTOR);
-
-//arrow
+const dataHere = document.querySelectorAll(AIR_DATEPICKER_SELECTOR);
 const arrowInput = document.querySelectorAll(AIR_DATEPICKER_ARROW_SELECTOR);
 
-const openDatepicker = () => {
-    dataHere.classList.toggle(AIR_DATEPICKER_VISIBLE_SELECTOR);
-    arrowInput.forEach((arrowAnim) => {
-        arrowAnim.classList.toggle(ARROW_ANIMATION);
+const openDatepicker = (event) => {
+    const calendarWrapper = event.target.closest(AIR_DATEPICKER_SELECTOR);
+    console.log("calendarWrapper", calendarWrapper);
+    const arrow = calendarWrapper.querySelector(AIR_DATEPICKER_ARROW_SELECTOR);
+    console.log("arrow", arrow);
+    const isOpen = calendarWrapper.classList.contains(AIR_DATEPICKER_VISIBLE_SELECTOR);
 
-    });
+    dataHere.forEach(dateOpen => dateOpen.classList.remove(AIR_DATEPICKER_VISIBLE_SELECTOR));
+
+    arrowInput.forEach((arrowAnim) => { arrowAnim.classList.remove(ARROW_ANIMATION); });
+
+    if (!isOpen) {
+        calendarWrapper.classList.add(AIR_DATEPICKER_VISIBLE_SELECTOR);
+        arrow.classList.add(ARROW_ANIMATION);
+    }
+    // dataHere.classList.toggle(AIR_DATEPICKER_VISIBLE_SELECTOR);
+    // dataHere.forEach(dateOpen => {
+    //     dateOpen.classList.toggle(AIR_DATEPICKER_VISIBLE_SELECTOR);
+    // });
+    // arrowInput.forEach((arrowAnim) => {
+    //     arrowAnim.classList.toggle(ARROW_ANIMATION);
+
+    // });
+
+
 };
 
 
@@ -38,50 +55,61 @@ dropDatapicker.addEventListener('click', (e) => {
 
 // Открытие через 2 input
 const fields = document.querySelectorAll(DATEPICKER_INPUT_SELECTOR);
-fields.forEach(field => field.addEventListener('click', openDatepicker));
+fields.forEach(field => {
+    const calendarWrapper = field.closest(AIR_DATEPICKER_SELECTOR);
+    console.log("calendarWrapper in fields.forEach", calendarWrapper);
+    if (calendarWrapper) {
+        field.addEventListener('click', openDatepicker);
+    }
+});
 
 
 // клик снаружи
-const outClick = document.querySelectorAll(DATEPICKER_INPUT_SELECTOR);
 document.addEventListener('click', (e) => {
-    if (e.target !== outClick) {
-        dataHere.classList.remove(AIR_DATEPICKER_VISIBLE_SELECTOR);
+    const isClickInside = dropDatapicker.contains(e.target);
+    if (!isClickInside) {
+        dataHere.forEach(calendar => {
+            calendar.classList.remove(AIR_DATEPICKER_VISIBLE_SELECTOR);
+        });
         arrowInput.forEach((arrowAnim) => {
             arrowAnim.classList.remove(ARROW_ANIMATION);
-
         });
     }
 });
 
 
+dataHere.forEach(calendar => {
+    const inputs = calendar.querySelectorAll(DATEPICKER_INPUT_SELECTOR);
+    const clearButton = calendar.querySelectorAll('.air-datepicker-button');
+    const datepick = new AirDatepicker(calendar, {
+        minDate: new Date(),
+        inline: true,
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: ['clear', {
+            content: "Применить",
+            onClick: openDatepicker,
+        }],
 
 
-
-const datepick = new AirDatepicker(AIR_DATEPICKER_SELECTOR, {
-    minDate: new Date(),
-    inline: true,
-    range: true,
-    multipleDatesSeparator: ' - ',
-    buttons: ['clear', {
-        content: "Применить",
-        onClick: openDatepicker,
-    }],
+        onSelect(date) {
+            const firstDate = document.querySelector('#startDate');
+            const lastDate = document.querySelector('#endDate');
+            const { formattedDate } = date;
+            const [start, end] = formattedDate;
+            if (start && end) {
+                firstDate.value = start;
+                lastDate.value = end;
 
 
-    onSelect(date) {
-        const firstDate = document.querySelector('#startDate');
-        const lastDate = document.querySelector('#endDate');
-        const { formattedDate } = date;
-        const [start, end] = formattedDate;
-        if (start && end) {
-            firstDate.value = start;
-            lastDate.value = end;
-
-
+            }
         }
-    }
 
+    });
 });
+
+
+
 
 const clearButton = document.querySelector('.air-datepicker-button');
 clearButton.addEventListener('click', () => {
