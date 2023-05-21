329448e45d46c8f69e5d9a47a03dfd252ddae6c9
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-05-21 14:50:51 +0300

    fix: field datepicker

--- src/views/components/inputs_fields/input_date.ts
+++ src/views/components/inputs_fields/input_date.ts
@@ -1,58 +1,61 @@
-// import AirDatepicker from 'air-datepicker';
-// import 'air-datepicker/air-datepicker.css';
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
 
-// const INPUT_DATEPICKER_BITHD = '#inputBirth';
-// const DATEPICKER_WRAPPER_BITHD = '.input-wrapper__bithd';
-// const DATEPICKER_WRAPPER_BITHD_OPENED = 'wrapper__bithd__opened';
+const INPUT_DATEPICKER_BITHD = '#inputBirth';
+const DATEPICKER_WRAPPER_BITHD = '.input-wrapper__bithd';
+const DATEPICKER_WRAPPER_BITHD_OPENED = 'wrapper__bithd__opened';
 
-// class DatepickerInput {
-//     constructor() {
-//         this.init();
-//     }
-//     init() {
-//         this.setControls();
-//         this.openDatepicker();
-//         this.datepickerOptions();
-//         this.handleDocumentClick();
-//     }
+class DatepickerInput {
+    private fieldDatepicker: HTMLElement | undefined;
+    private datepickerWrapper: HTMLElement | undefined
 
-//     setControls() {
-//         this.fieldDatepicker = document.querySelector(INPUT_DATEPICKER_BITHD);
-//         this.datepickerWrapper = document.querySelector(DATEPICKER_WRAPPER_BITHD);
-//     }
+    constructor() {
+        this.init();
+    }
+    init() {
+        this.setControls();
+        this.openDatepicker();
+        this.datepickerOptions();
+        this.handleDocumentClick();
+    }
 
-//     openDatepicker() {
-//         this.fieldDatepicker.addEventListener('click', () => {
-//             this.datepickerWrapper.classList.toggle(DATEPICKER_WRAPPER_BITHD_OPENED);
-//         });
-//     }
+    setControls() {
+        this.fieldDatepicker = document.querySelector(INPUT_DATEPICKER_BITHD);
+        this.datepickerWrapper = document.querySelector(DATEPICKER_WRAPPER_BITHD);
+    }
 
-//     handleDocumentClick() {
-//         document.addEventListener('click', (event) => {
-//             const target = event.target;
-//             if (!this.datepickerWrapper.contains(target) && !this.fieldDatepicker.contains(target)) {
-//                 this.datepickerWrapper.classList.remove(DATEPICKER_WRAPPER_BITHD_OPENED);
-//             }
-//         });
-//     }
+    openDatepicker() {
+        this.fieldDatepicker.addEventListener('click', () => {
+            this.datepickerWrapper.classList.toggle(DATEPICKER_WRAPPER_BITHD_OPENED);
+        });
+    }
 
-//     datepickerOptions() {
-//         const inputFieldBithd = new AirDatepicker(DATEPICKER_WRAPPER_BITHD, {
-//             autoClose: true,
-//             buttons: [{
-//                 content: "Применить",
-//                 onClick: () => {
-//                     this.datepickerWrapper.classList.remove(DATEPICKER_WRAPPER_BITHD_OPENED);
-//                 },
-//             }],
+    handleDocumentClick() {
+        document.addEventListener('click', (event) => {
+            const target = event.target;
+            if (!this.datepickerWrapper.contains(target as Node) && !this.fieldDatepicker.contains(target as Node)) {
+                this.datepickerWrapper.classList.remove(DATEPICKER_WRAPPER_BITHD_OPENED);
+            }
+        });
+    }
 
-//             onSelect: (date) => {
-//                 const { formattedDate } = date;
-//                 const fieldId = document.querySelector(INPUT_DATEPICKER_BITHD);
-//                 fieldId.value = formattedDate;
-//             }
-//         });
-//     }
-// }
+    datepickerOptions() {
+        const inputFieldBithd = new AirDatepicker(DATEPICKER_WRAPPER_BITHD, {
+            autoClose: true,
+            buttons: [{
+                content: "Применить",
+                onClick: () => {
+                    this.datepickerWrapper.classList.remove(DATEPICKER_WRAPPER_BITHD_OPENED);
+                },
+            }],
 
-// const datepickerInput = new DatepickerInput();
+            onSelect: (date) => {
+                const { formattedDate } = date;
+                const fieldId = document.querySelector(INPUT_DATEPICKER_BITHD) as HTMLInputElement;
+                fieldId.value = formattedDate as string;
+            }
+        });
+    }
+}
+
+const datepickerInput = new DatepickerInput();
