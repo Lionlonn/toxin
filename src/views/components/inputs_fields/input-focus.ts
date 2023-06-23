1bfc045d024ac6234833024f0c56e16c54068c2c
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2023-06-23 15:47:26 +0300

    fix error inputs.ts

---
+++ src/views/components/inputs_fields/input-focus.ts
@@ -0,0 +1,2 @@
+const test = document.querySelector('[data-focus]') as HTMLInputElement
+test.focus()
--- src/views/pages/form-elements/index.ts
+++ src/views/pages/form-elements/index.ts
@@ -1,1 +1,10 @@
-// import '~form-elements.scss'
+import 'components/inputs_fields/input_date.ts'
+import 'components/inputs_fields/input-focus.ts'
+import 'components/dropdown_guests/index.ts';
+import 'components/datepicker-card/date-card.ts'
+import 'components/conveniences/index.ts'
+
+import './form-elements.scss'
+
+
+
